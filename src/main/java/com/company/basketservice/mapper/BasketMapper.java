682e136b0643b5829b48942bf9b4e9695b7829bb
Muhammadamin <muhammadaminr8@gmail.com>
2023-07-19 12:51:04 +0500

    Changed mapper: modified to mapstruct

--- src/main/java/com/company/basketservice/mapper/BasketMapper.java
+++ src/main/java/com/company/basketservice/mapper/BasketMapper.java
@@ -3,62 +3,27 @@ package com.company.basketservice.mapper;
 
 import com.company.basketservice.dto.BasketDto;
 import com.company.basketservice.module.Basket;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.Mapping;
+
 
 @Component
+@Mapper(componentModel = "spring")
 public abstract class BasketMapper {
 
-    public Basket toEntity(BasketDto dto) {
-        Basket basket = new Basket();
-        basket.setId(dto.getId());
-        basket.setProdMass(dto.getProdMass());
-        basket.setProdPrice(dto.getProdPrice());
-        basket.setTotalPrice(dto.getTotalPrice());
-        /*basket.setCreatedAt(dto.getCreatedAt());
-        basket.setUpdatedAt(dto.getUpdatedAt());
-        basket.setDeletedAt(dto.getDeletedAt());*/
-        return basket;
-    }
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "deletedAt", ignore = true)
+    public abstract Basket toEntity(BasketDto dto);
 
-    public BasketDto toDto(Basket basket) {
-        BasketDto dto = new BasketDto();
-        dto.setId(basket.getId());
-        dto.setProdMass(basket.getProdMass());
-        dto.setProdPrice(basket.getProdPrice());
-        dto.setTotalPrice(basket.getTotalPrice());
-        /*dto.setCreatedAt(basket.getCreatedAt());
-        dto.setUpdatedAt(basket.getUpdatedAt());
-        dto.setDeletedAt(basket.getDeletedAt());*/
-        return dto;
-    }
+    public abstract BasketDto toDto(Basket basket);
 
-    public Basket updateBasketFromDto(BasketDto dto, Basket basket) {
-        if (dto == null) {
-            return basket;
-        }
-        if (dto.getId() != null) {
-            basket.setId(dto.getId());
-        }
-        if (dto.getProdMass() != null) {
-            basket.setProdMass(dto.getProdMass());
-        }
-        if (dto.getProdPrice() != null) {
-            basket.setProdPrice(dto.getProdPrice());
-        }
-        if (dto.getTotalPrice() != null) {
-            basket.setTotalPrice(dto.getTotalPrice());
-        }
-        if (dto.getCreatedAt() != null) {
-            basket.setCreatedAt(dto.getCreatedAt());
-        }
-        if (dto.getUpdatedAt() != null) {
-            basket.setUpdatedAt(dto.getUpdatedAt());
-        }
-        if (dto.getDeletedAt() != null) {
-            basket.setDeletedAt(dto.getDeletedAt());
-        }
-
-        return basket;
-    }
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "deletedAt", ignore = true)
+    public abstract Basket updateBasketFromDto(BasketDto dto, @MappingTarget Basket basket);
 }
--- src/main/java/com/company/basketservice/mapper/LoanerMapper.java
+++ src/main/java/com/company/basketservice/mapper/LoanerMapper.java
@@ -4,56 +4,25 @@ package com.company.basketservice.mapper;
 
 import com.company.basketservice.dto.LoanerDto;
 import com.company.basketservice.module.Loaner;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 import org.springframework.stereotype.Component;
 
 @Component
 public abstract class LoanerMapper {
 
-    public Loaner toEntity(LoanerDto dto) {
-        Loaner loaner = new Loaner();
-        loaner.setId(dto.getId());
-        loaner.setTotalPrice(dto.getTotalPrice());
-        loaner.setStatus(dto.getStatus());
-        loaner.setCreatedAt(dto.getCreatedAt());
-        loaner.setUpdatedAt(dto.getUpdatedAt());
-        loaner.setDeletedAt(dto.getDeletedAt());
-        return loaner;
-    }
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "deletedAt", ignore = true)
+    public abstract Loaner toEntity(LoanerDto dto);
 
-    public LoanerDto toDto(Loaner loaner) {
-        LoanerDto dto = new LoanerDto();
-        dto.setId(loaner.getId());
-        dto.setTotalPrice(loaner.getTotalPrice());
-        dto.setStatus(loaner.getStatus());
-        dto.setCreatedAt(loaner.getCreatedAt());
-        dto.setUpdatedAt(loaner.getUpdatedAt());
-        dto.setDeletedAt(loaner.getDeletedAt());
-        return dto;
-    }
+    public abstract LoanerDto toDto(Loaner loaner);
 
-    public Loaner updateLoanerFromDto(LoanerDto dto, Loaner loaner) {
-        if (dto == null) {
-            return loaner;
-        }
-        if (dto.getId() != null) {
-            loaner.setId(dto.getId());
-        }
-        if (dto.getTotalPrice() != null) {
-            loaner.setTotalPrice(dto.getTotalPrice());
-        }
-        if (dto.getStatus() != null) {
-            loaner.setStatus(dto.getStatus());
-        }
-        if (dto.getCreatedAt() != null) {
-            loaner.setCreatedAt(dto.getCreatedAt());
-        }
-        if (dto.getUpdatedAt() != null) {
-            loaner.setUpdatedAt(dto.getUpdatedAt());
-        }
-        if (dto.getDeletedAt() != null) {
-            loaner.setDeletedAt(dto.getDeletedAt());
-        }
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "deletedAt", ignore = true)
+    public abstract Loaner updateLoanerFromDto(LoanerDto dto, @MappingTarget Loaner loaner);
 
-        return loaner;
-    }
 }
